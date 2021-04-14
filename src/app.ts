4f1e8425d5a46e35a6a1d6db00d983cad87e6c7b
Igor Voronin <igor.voronin@cegeka.de>
2021-04-14 22:19:13 +0200

    npm pack added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
+const { version } = require("../.appversion.json");
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send(`Hello World (v${version})!`)
 })
 
 app.listen(port, () => {
